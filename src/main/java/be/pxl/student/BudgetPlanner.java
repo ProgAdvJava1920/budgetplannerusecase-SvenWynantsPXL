b665048d104cf11f5b35a075530a39872521f288
Sven Wynants <sven.wynants@hotmail.com>
2020-03-04 12:34:01 +0100

    log4j2

--- src/main/java/be/pxl/student/BudgetPlanner.java
+++ src/main/java/be/pxl/student/BudgetPlanner.java
@@ -1,6 +1,37 @@
 package be.pxl.student;
 
-public class BudgetPlanner {
+import be.pxl.student.entity.Account;
+import be.pxl.student.util.BudgetPlannerException;
+import be.pxl.student.util.BudgetPlannerImporter;
+import be.pxl.student.util.BudgetPlannerMapper;
 
+import java.nio.file.Paths;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class BudgetPlanner {
+    private static Logger logger = LogManager.getLogger(BudgetPlanner.class);
+
+    public static void main(String[] args) {
+
+        String csvFile = "src/main/resources/account_payments.csv";
+
+        try{
+            logger.info("Starting csv import");
+            List<String> list = BudgetPlannerImporter.readCsvFile(Paths.get(csvFile));
+            logger.info("Csv import done");
+
+            logger.info("Starting account mapping");
+            List<Account> accounts = new BudgetPlannerMapper().mapAccounts(list);
+            accounts.forEach(logger::debug);
+            logger.info("Account mapping done");
+        } catch (BudgetPlannerException e){
+            e.printStackTrace();
+        }
+
+
+    }
 
 }
